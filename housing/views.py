370846eeefdad9611ddfdd9ca0c7291e2a7359ce
harsimran-s20 <s388232@students.cdu.edu.au>
2026-04-12 16:15:54 +0930

    Add repair request CRUD views and templates
    Introduce Create/Update/Delete views for RepairRequest (CreateView, UpdateView, DeleteView) with success_url using reverse_lazy to 'request-list'. Update ListView and DetailView to use new template names (request_list.html, request_detail.html) and import RepairRequestForm from the forms module. Add simple templates for request_form.html and request_confirm_delete.html and create a new housing/form.py placeholder to host the form class.

--- housing/views.py
+++ housing/views.py
@@ -1,10 +1,12 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import RepairRequest
+from .forms import RepairRequestForm
 
 
 class RepairRequestListView(ListView):
     model = RepairRequest
-    template_name = "housing/repairrequest_list.html"
+    template_name = "housing/request_list.html"
     context_object_name = "repair_requests"
 
     def get_queryset(self):
@@ -17,11 +19,28 @@ class RepairRequestListView(ListView):
 
 class RepairRequestDetailView(DetailView):
     model = RepairRequest
-    template_name = "housing/repairrequest_detail.html"
+    template_name = "housing/request_detail.html"
     context_object_name = "repair_request"
 
     def get_queryset(self):
-        return (
-            RepairRequest.objects
-            .select_related("dwelling", "tenant")
-        )
+        return RepairRequest.objects.select_related("dwelling", "tenant")
+
+
+class RepairRequestCreateView(CreateView):
+    model = RepairRequest
+    form_class = RepairRequestForm
+    template_name = "housing/request_form.html"
+    success_url = reverse_lazy("request-list")
+
+
+class RepairRequestUpdateView(UpdateView):
+    model = RepairRequest
+    form_class = RepairRequestForm
+    template_name = "housing/request_form.html"
+    success_url = reverse_lazy("request-list")
+
+
+class RepairRequestDeleteView(DeleteView):
+    model = RepairRequest
+    template_name = "housing/request_confirm_delete.html"
+    success_url = reverse_lazy("request-list")
